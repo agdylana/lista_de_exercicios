19d683c6a1390d2b6f69bf3b84e1eb338b1d2e70
Agylannah F <>
2022-08-10 19:47:30 +0000

    Initial commit

---
+++ questao03.py
@@ -0,0 +1,11 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = float(input('digite um numero: '))
+numero2 = float(input('digite outro numero: '))
+
+soma = numero1 + numero2
+
+print('A soma é:', soma)
+
+
+
